caf47a9a1252bce685efdc3d88184dce86c38f26
David PETIPAS <David@imac.local>
2018-09-02 21:37:21 +0200

    Ajout classe test

---
+++ Test du Source Control/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by David PETIPAS on 02/09/2018.
+//  Copyright © 2018 David PETIPAS. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut() {
+        print ("Salut les codeurs")
+    }
+}
